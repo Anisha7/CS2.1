a64290c992ba9377844e8701e4e176b53cc2f46f
Anisha Jain <anishajain@outlook.com>
2019-02-05 15:52:35 -0800

    shortcuts

--- code/sorting_integer.py
+++ code/sorting_integer.py
@@ -1,6 +1,9 @@
 #!python
 
-
+    # [5,6,7,8,9]
+    # 9-5 = 4
+    # [30,40,95,100]
+    # 100-30 = 70
 def counting_sort(numbers):
     """Sort given numbers (integers) by counting occurrences of each number,
     then looping over counts and copying that many numbers into output list.
@@ -10,22 +13,16 @@ def counting_sort(numbers):
     print('input: %s'%(str(numbers)))
     minNum = min(numbers)
     maxNum = max(numbers)
-    listLen = maxNum - minNum
+    listLen = maxNum - minNum + 1
     # Create list of counts with a slot for each number in input range
-    countList = []
-    for i in range(listLen+1):
-        countList.append(0)
+    countList = [0]*listLen
     # Loop over given numbers and increment each number's count
-    # [5,6,7,8,9]
-    # 9-5 = 4
-    # [30,40,95,100]
-    # 100-30 = 70
-    while len(numbers)>0:
+    while len(numbers)>0: # O(N)
         num = numbers.pop()
         i = num - minNum
         countList[i] += 1
     # Loop over counts and append that many numbers into output list
-    for i in range(len(countList)):
+    for i in range(len(countList)): # O(N)
         count = countList[i]
         while count > 0:
             numbers.append(i + minNum)
@@ -34,6 +31,12 @@ def counting_sort(numbers):
     return numbers
 
 
+    # [15,20,25,30,33,44,50]
+    # 50-15 = 35
+    # 35//9 + 1 = 4
+    # 15 + 4 = 19, everything less than 18 in one bucket
+    # 15,   19,   23,   27,   31,   35, 39,   43,   47, 51,
+    #   [15], [20], [25], [30], [33], [], [44], [50], [], []
 def bucket_sort(numbers, num_buckets=10):
     """Sort given numbers by distributing into buckets representing subranges,
     then sorting each bucket and concatenating all buckets in sorted order.
@@ -45,12 +48,6 @@ def bucket_sort(numbers, num_buckets=10):
     maxNum = max(numbers)
     totalRange = maxNum - minNum
     # Create list of buckets to store numbers in subranges of input range
-    # [15,20,25,30,33,44,50]
-    # 50-15 = 35
-    # 35//9 + 1 = 4
-    # 15 + 4 = 19, everything less than 18 in one bucket
-    # 15,   19,   23,   27,   31,   35, 39,   43,   47, 51,
-    #   [15], [20], [25], [30], [33], [], [44], [50], [], []
     bucket = []
     for i in range(num_buckets):
         bucket.append([])
@@ -83,7 +80,7 @@ if __name__ == '__main__':
     counting_sort(L2)
     print(L2)
     L3 = [4,7,2,8,1,3,2,5]
-    bucket_sort(L3)
+    bucket_sort(L3, 3)
     print(L3)
     L4 = [4,7,2,8,1,3,2,5,6,7,3,436,22,2,45,67]
     bucket_sort(L4)
